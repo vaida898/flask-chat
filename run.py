caf29760da917b976f634fb8b8a3933350d23c8c
vaida898 <freelpfm@gmail.com>
2020-07-20 09:19:17 +0000

    create empty list to store messages

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+messages = []
+
+
+def add_messages(username, message):
+    messages.append("{}: {}".format(username, message))
 
 
 @app.route('/')
@@ -12,11 +17,13 @@ def index():
 
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """" Display chat messages """
+    return "Welcome, {0}".format(username, messages)
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
+    """ Create a new message and redirect back to the chat page """
     return "{0}: {1}".format(username, message)
 
 
