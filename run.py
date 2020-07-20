39c8180dca10e7ae071478fb5f9b536aaeac70ef
vaida898 <freelpfm@gmail.com>
2020-07-20 11:53:31 +0000

    added ability for user to write messages and display them on screen

--- run.py
+++ run.py
@@ -1,30 +1,37 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 messages = []
 
 
 def add_messages(username, message):
+    """Add messages to the `messages` list"""
     messages.append("{}: {}".format(username, message))
 
 
-@app.route('/')
+def get_all_messages():
+    """Get all of the messages and separate them with a `br`"""
+    return "<br>".join(messages)
+
+
+@app.route("/")
 def index():
-    """ Main page with instructions """
-    return "To send a message use /USERNAME/MESSAGE"
+    """Main page with instructions"""
+    return "To send a message use: /USERNAME/MESSAGE"
 
 
-@app.route('/<username>')
+@app.route("/<username>")
 def user(username):
-    """" Display chat messages """
-    return "Welcome, {0}".format(username, messages)
+    """Display chat messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 
-@app.route('/<username>/<message>')
+@app.route("/<username>/<message>")
 def send_message(username, message):
-    """ Create a new message and redirect back to the chat page """
-    return "{0}: {1}".format(username, message)
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
