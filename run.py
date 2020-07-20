e71269b4b364e0af8dd9ef2ecab84d51c16e95a1
vaida898 <freelpfm@gmail.com>
2020-07-20 12:54:33 +0000

    added session variables to store user names

--- run.py
+++ run.py
@@ -1,14 +1,16 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
+
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
 
 
 def add_messages(username, message):
-    now = datetime.now().strftime("%H:%M:%S")
     """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
     messages.append("({}) {}: {}".format(now, username, message))
 
 
@@ -17,9 +19,15 @@ def get_all_messages():
     return "<br>".join(messages)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect(session["username"])
+
     return render_template("index.html")
 
 
